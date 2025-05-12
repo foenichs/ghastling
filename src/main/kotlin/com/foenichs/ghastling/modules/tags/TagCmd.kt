4f432f55e5216b7e034e1bf94bfb347866134abd
foenichs <foenichs3000@gmail.com>
2025-05-12 23:56:56 +0200

    Tag Add command implemented

--- src/main/kotlin/com/foenichs/ghastling/modules/tags/TagCmd.kt
+++ src/main/kotlin/com/foenichs/ghastling/modules/tags/TagCmd.kt
@@ -1,10 +1,9 @@
 package com.foenichs.ghastling.modules.tags
 
 import com.foenichs.ghastling.utils.entities.SlashCommandEvent
-import com.foenichs.ghastling.utils.sql.SQL
-import dev.minn.jda.ktx.interactions.components.Container
-import dev.minn.jda.ktx.interactions.components.TextDisplay
+import dev.minn.jda.ktx.interactions.components.*
 import dev.minn.jda.ktx.messages.reply_
+import net.dv8tion.jda.api.components.textinput.TextInputStyle
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
 object TagCmd : SlashCommandEvent {
@@ -17,8 +16,19 @@ object TagCmd : SlashCommandEvent {
                         useComponentsV2 = true,
                         components = listOf(
                             Container {
-                                accentColor = 0x9FAA9F
-                                +TextDisplay("The tag name is too long, it has to be less than 32 characters.")
+                                +TextDisplay("The tag name is **too long**, it has to be less than 32 characters.")
+                            },
+                        ),
+                        ephemeral = true,
+                    ).queue()
+                    return
+                }
+                if (!tagName.matches(Regex("^[a-zA-Z ]*\$"))) {
+                    it.reply_(
+                        useComponentsV2 = true,
+                        components = listOf(
+                            Container {
+                                +TextDisplay("The tag name can't contain any **special characters**.")
                             },
                         ),
                         ephemeral = true,
@@ -26,59 +36,59 @@ object TagCmd : SlashCommandEvent {
                     return
                 }
 
-                val (channelId, messageId) = it.getOption("message")?.asString?.split("/")?.takeLast(2) ?: listOf(
-                    null, null
-                )
-                val channel = channelId?.let { it1 -> it.guild?.getTextChannelById(it1) }
-                val message = messageId?.let { it1 -> channel?.retrieveMessageById(it1)?.complete() } ?: return
 
-                SQL.call("INSERT INTO tags (guild_id, tag_name, content, embeds, cv2, is_ephemeral, components) VALUES (?, ?, ?, ?, ?, ?, ?)") {
-                    setLong(1, it.guild?.idLong ?: return)
-                    setString(2, it.getOption("name")?.asString ?: return)
-                    setString(3, message.contentRaw)
-                    setString(4, message.embeds.toString())
-                    setBoolean(5, message.isUsingComponentsV2)
-                    setBoolean(6, message.isEphemeral)
-                    setString(7, message.components.toString())
-                }
-                it.reply_(
-                    useComponentsV2 = true,
-                    components = listOf(
-                        Container {
-                            accentColor = 0xB6C8B5
-                            +TextDisplay(
-                                "The tag **$tagName** was successfully added, you can now use it with `?t $tagName`."
-                            )
-                        },
-                    ),
-                    ephemeral = true,
+                it.replyModal(
+                    Modal("tagAddModal:$tagName", "Add the $tagName tag") {
+                        builder.addComponents(
+                            ActionRow {
+                                +TextInput("tagTitle", "Title", TextInputStyle.SHORT) {
+                                    required = false
+                                    requiredLength = 0..256
+//                                    placeholder = "Displayed on top of the tag embed."
+                                }
+                            },
+                            ActionRow {
+                                +TextInput("tagDescription", "Description", TextInputStyle.PARAGRAPH) {
+                                    required = false
+//                                    placeholder = "Normal sized text below the title."
+                                }
+                            },
+                            ActionRow {
+                                +TextInput("tagImageUrl", "Image URL", TextInputStyle.SHORT) {
+                                    required = false
+//                                    placeholder = "The url of a large image in the tag embed."
+                                }
+                            },
+                            ActionRow {
+                                +TextInput("tagColor", "Color (HEX)", TextInputStyle.SHORT) {
+                                    required = false
+                                    requiredLength = 0..7
+                                    placeholder = "#B6C8B5"
+                                }
+                            }
+                        )
+                    },
                 ).queue()
             }
 
             "send" -> {
-                val resultSet =
-                    SQL.call("SELECT guild_id, tag_name, content, embeds, cv2, is_ephemeral, components FROM tags WHERE guild_id = ? AND tag_name = ?") {
-                        setLong(1, it.guild?.idLong ?: return)
-                        setString(2, it.getOption("name")?.asString ?: return)
-                    }
-
-                while (resultSet.next()) {
-                    val guildId = resultSet.getLong("guild_id")
-                    val tagName = resultSet.getString("tag_name")
-                    val content = resultSet.getString("content")
-                    val embeds = resultSet.getString("embeds")
-                    val cv2 = resultSet.getBoolean("cv2")
-                    val isEphemeral = resultSet.getBoolean("is_ephemeral")
-                    val components = resultSet.getString("components")
-
+//                val resultSet =
+//                    SQL.call("SELECT guild_id, tag_name, components FROM tags WHERE guild_id = ? AND tag_name = ?") {
+//                        setLong(1, it.guild?.idLong ?: return)
+//                        setString(2, it.getOption("name")?.asString ?: return)
+//                    }
+//
+//                while (resultSet.next()) {
+//                    val componentsJson = resultSet.getString("components")
+//                    val components = kotlin.runCatching {
+//                        json.decodeFromString<List<Component>>(componentsJson)
+//                    }.getOrNull()?.map { it.toDiscord() }
+//
 //                    it.reply_(
-//                        content = content,
-// !!                     embeds = message.embeds.toCollection(mutableListOf()),
-//                        useComponentsV2 = cv2,
-//                        ephemeral = isEphemeral,
-// !!                     components = message.components.toCollection(mutableListOf()),
+//                        useComponentsV2 = true,
+//                        components = components,
 //                    ).queue()
-                }
+//                }
             }
         }
     }
--- src/main/kotlin/com/foenichs/ghastling/modules/tags/TagCmdModals.kt
+++ src/main/kotlin/com/foenichs/ghastling/modules/tags/TagCmdModals.kt
@@ -1,6 +1,7 @@
 package com.foenichs.ghastling.modules.tags
 
 import com.foenichs.ghastling.utils.entities.ModalEvent
+import com.foenichs.ghastling.utils.sql.SQL
 import dev.minn.jda.ktx.interactions.components.Container
 import dev.minn.jda.ktx.interactions.components.TextDisplay
 import dev.minn.jda.ktx.messages.reply_
@@ -11,22 +12,38 @@ object TagCmdModals : ModalEvent {
         val baseModalId = it.modalId.split(":")[0]
         when (baseModalId) {
             "tagAddModal" -> {
-                val tagname = it.modalId.split(":").getOrNull(1)
+                val tagName = it.modalId.split(":").getOrNull(1)
+
+                val tagTitle = it.getValue("tagTitle")?.asString
+                val tagDescription = it.getValue("tagDescription")?.asString
+                val tagImageUrl = it.getValue("tagImageUrl")?.asString
+                val tagColor = it.getValue("tagColor")?.asString ?: "0xB6C8B5"
+
+                SQL.call("INSERT INTO tags (guildId, tagName, title, description, imageUrl, color) VALUES (?, ?, ?, ?, ?, ?);") {
+                    setLong(1, it.guild?.idLong ?: return)
+                    setString(2, tagName)
+                    setString(3, tagTitle)
+                    setString(4, tagDescription)
+                    setString(5, tagImageUrl)
+                    setString(6, tagColor)
+                }
+
+                val prefix = SQL.call("SELECT prefix FROM guildIndex WHERE guildId = ?") {
+                    setLong(1, it.guild?.idLong ?: return)
+                }.use { resultSet ->
+                    if (resultSet.next()) resultSet.getString("prefix") else null
+                }
+
                 it.reply_(
                     useComponentsV2 = true,
                     components = listOf(
                         Container {
                             accentColor = 0xB6C8B5
-                            uniqueId = 1
-                            +TextDisplay("The **$tagname** tag was successfully added, you can now use it with `?t $tagname`.")
+                            +TextDisplay("The **$tagName** tag was successfully added, you can now use it with `$prefix$tagName`.")
                         },
                     ),
                     ephemeral = true,
                 ).queue()
-                val tagContent = it.getValue("tagContent")?.asString
-                val tagTitle = it.getValue("tagTitle")?.asString
-                val tagDescription = it.getValue("tagDescription")?.asString
-                val tagColor = it.getValue("tagColor")?.asString?.toIntOrNull() ?: 0xB6C8B5
             }
         }
     }
