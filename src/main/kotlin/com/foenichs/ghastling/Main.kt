8ac1b52e2752438ea98513dc6b1451269635cdcf
foenichs <foenichs3000@gmail.com>
2025-05-14 23:27:32 +0200

    First basic tag triggering

--- src/main/kotlin/com/foenichs/ghastling/Main.kt
+++ src/main/kotlin/com/foenichs/ghastling/Main.kt
@@ -3,6 +3,7 @@ package com.foenichs.ghastling
 import com.foenichs.ghastling.config.Config
 import com.foenichs.ghastling.modules.tags.LegacyTags
 import com.foenichs.ghastling.modules.tags.TagCmdModals
+import com.foenichs.ghastling.modules.tags.TagTriggering
 import com.foenichs.ghastling.utility.ServerInitialisation
 import com.foenichs.ghastling.utils.manager.ButtonManager
 import com.foenichs.ghastling.utils.manager.DropDownManager
@@ -58,6 +59,8 @@ object Ghastling {
         ServerInitialisation
         TagCmdModals
         LegacyTags
+
+        TagTriggering
 
         println("[Ghastling] The application started successfully!")
     }
---
+++ src/main/kotlin/com/foenichs/ghastling/modules/tags/TagTriggering.kt
@@ -0,0 +1,58 @@
+package com.foenichs.ghastling.modules.tags
+
+import com.foenichs.ghastling.Ghastling
+import com.foenichs.ghastling.utils.sql.SQL
+import dev.minn.jda.ktx.events.listener
+import dev.minn.jda.ktx.interactions.components.Container
+import dev.minn.jda.ktx.interactions.components.MediaGallery
+import dev.minn.jda.ktx.interactions.components.TextDisplay
+import dev.minn.jda.ktx.messages.MentionConfig
+import dev.minn.jda.ktx.messages.Mentions
+import dev.minn.jda.ktx.messages.send
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+
+object TagTriggering {
+    val onMessage = Ghastling.JDA.listener<MessageReceivedEvent> {
+        val guildId = it.guild.idLong
+        val message = it.message
+        val messageContent = message.contentRaw
+        val channel = it.channel
+
+        val result = SQL.call("SELECT prefix FROM guildIndex WHERE guildId = ?") {
+            setLong(1, guildId)
+        }
+        if (result.next()) {
+            val prefix = result.getString("prefix")
+            if (messageContent.startsWith(prefix)) {
+                val arguments = messageContent.removePrefix(prefix).trim()
+                val tagName = arguments.split(Regex("\\s+")).firstOrNull() ?: return@listener
+                val tagResult =
+                    SQL.call("SELECT title, description, imageUrl, color FROM tags WHERE guildId = ? AND tagName = ?") {
+                        setLong(1, guildId)
+                        setString(2, tagName)
+                    }
+                if (tagResult.next()) {
+                    channel.send(
+                        useComponentsV2 = true,
+                        components = listOf(
+                            Container {
+                                accentColor = tagResult.getString("color")?.toIntOrNull(16)
+                                tagResult.getString("title")?.let { +TextDisplay("### " + it) }
+                                tagResult.getString("description")?.let { +TextDisplay(it) }
+                                tagResult.getString("imageUrl")?.let {
+                                    +MediaGallery {
+                                        item(it)
+                                    }
+                                }
+                            }), mentions = Mentions(
+                            MentionConfig.users(emptyList()),
+                            MentionConfig.roles(emptyList()),
+                            everyone = false,
+                            here = false,
+                        )
+                    ).queue()
+                }
+            }
+        }
+    }
+}
