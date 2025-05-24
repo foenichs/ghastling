1d8bde5f0b8813caa66a16eb7418bf5ba4eab817
foenichs <foenichs3000@gmail.com>
2025-05-24 21:37:11 +0200

    Added help command

---
+++ src/main/kotlin/com/foenichs/ghastling/utility/HelpCmd.kt
@@ -0,0 +1,55 @@
+package com.foenichs.ghastling.utility
+
+import com.foenichs.ghastling.utils.entities.SlashCommandEvent
+import com.foenichs.ghastling.utils.sql.SQL
+import dev.minn.jda.ktx.interactions.components.*
+import dev.minn.jda.ktx.messages.reply_
+import net.dv8tion.jda.api.components.separator.Separator.Spacing
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+object HelpCmd : SlashCommandEvent {
+    override suspend fun trigger(it: SlashCommandInteractionEvent) {
+        val guildId = it.guild?.idLong ?: return
+        val result = SQL.call("SELECT prefix FROM guildIndex WHERE guildId = ?") {
+            setLong(1, guildId)
+        }
+        val prefix = if (result.next()) {
+            result.getString("prefix")
+        } else {
+            "!"
+        }
+        it.reply_(
+            useComponentsV2 = true, components = listOf(Container {
+                accentColor = 0xB6C8B5
+//                    +MediaGallery {
+//                        item("https://i.imgur.com/TZw9gNu.png")
+//                    }
+                +TextDisplay(
+                    "-# <:ghastling:1367898099887902772> Ghastling"
+                )
+                +TextDisplay(
+                    "-# A cute little tag/faq bot that you can easily configure and use on your own server."
+                )
+                +Separator(true, spacing = Spacing.SMALL)
+                +TextDisplay(
+                    "**Tags** let you send long or important messages quickly, which is great for busy servers. Use the </tag:1373059281884024974> command to create, edit, remove, or list tags. You can also restrict tag usage to specific roles. Each tag has a 10-second cooldown per channel."
+                )
+                +Separator(false, spacing = Spacing.SMALL)
+                +TextDisplay(
+                    "Using </tag add:1373059281884024974> will open a menu to configure the embed sent when the tag is used. Alternatively, you can add content directly in the command option to send it as a regular message instead of an embed."
+                )
+                +Separator(false, spacing = Spacing.SMALL)
+                +TextDisplay(
+                    "The **prefix** of the server changes how members use tags. You can change it using </tag prefix:1373059281884024974>. Your current prefix is `$prefix`, so typing `${prefix}rules` will send the `rules` tag. Tag commands are deleted after the tag is sent, if the message doesn't contain other content. The tag command doesn't have to be at the start of the message."
+                )
+                +Separator(true, spacing = Spacing.SMALL)
+                +TextDisplay(
+                    "-# Feel free to report bugs or suggest features by opening an issue on GitHub."
+                )
+            }, ActionRow {
+                +link("https://github.com/foenichs/ghastling", "GitHub")
+                +link("https://discord.com/oauth2/authorize?client_id=1365745768383905923", "Invite me")
+            }), ephemeral = true
+        ).queue()
+    }
+}
--- src/main/kotlin/com/foenichs/ghastling/utils/manager/SlashCommandManager.kt
+++ src/main/kotlin/com/foenichs/ghastling/utils/manager/SlashCommandManager.kt
@@ -2,6 +2,7 @@ package com.foenichs.ghastling.utils.manager
 
 import com.foenichs.ghastling.Ghastling
 import com.foenichs.ghastling.modules.tags.TagCmd
+import com.foenichs.ghastling.utility.HelpCmd
 import dev.minn.jda.ktx.events.listener
 import dev.minn.jda.ktx.interactions.commands.Command
 import dev.minn.jda.ktx.interactions.commands.option
@@ -13,7 +14,8 @@ import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
 
 object SlashCommandManager {
     private val commands = mapOf(
-        "tag" to TagCmd
+        "tag" to TagCmd,
+        "help" to HelpCmd
     )
 
     // Logs command executions in the console
@@ -31,44 +33,56 @@ object SlashCommandManager {
         flame?.updateCommands()?.addCommands()?.queue()
 
         // Adds global commands
-        Ghastling.JDA.updateCommands().addCommands(Command("tag", "Manage tags for this server.") {
-            defaultPermissions = DefaultMemberPermissions.enabledFor(
-                Permission.MANAGE_SERVER
-            )
-            subcommand("add", "Adds a new tag for this server.") {
-                option<String>(
-                    "name", "The name to use and send this tag.", true
-                ) { setMaxLength(32) }
-                option<String>(
-                    "content", "Optional content, if you don't want to use an embed.", false
-                ) { setMaxLength(4000) }
-            }
-            subcommand("remove", "Removes a tag from this server.") {
-                option<String>(
-                    "name", "The name of the tag that will be removed.", true
-                ) { setMaxLength(32) }
-            }
-            subcommand("edit", "Edits a existing tag on this server.") {
-                option<String>(
-                    "name", "The name of the tag that will be edited.", true
-                ) { setMaxLength(32) }
-            }
-            subcommand("list", "Shows all tags of this server.")
-            subcommand("prefix", "Change the prefix to send tags.") {
-                option<String>(
-                    "prefix", "The prefix to send tags.", true
-                ) { setMaxLength(16) }
-                option<Boolean>(
-                    "space", "Add a space behind the prefix.", true
+        Ghastling.JDA.updateCommands().addCommands(
+            Command("tag", "Manage tags for this server.") {
+                defaultPermissions = DefaultMemberPermissions.enabledFor(
+                    Permission.MANAGE_SERVER
                 )
-            }
-            subcommand("permissions", "Changes which roles are allowed to send tags.")
+                subcommand("add", "Adds a new tag for this server.") {
+                    option<String>(
+                        "name", "The name to use and send this tag.", true
+                    ) { setMaxLength(32) }
+                    option<String>(
+                        "content", "Optional content, if you don't want to use an embed.", false
+                    ) { setMaxLength(4000) }
+                }
+                subcommand("remove", "Removes a tag from this server.") {
+                    option<String>(
+                        "name", "The name of the tag that will be removed.", true
+                    ) { setMaxLength(32) }
+                }
+                subcommand("edit", "Edits a existing tag on this server.") {
+                    option<String>(
+                        "name", "The name of the tag that will be edited.", true
+                    ) { setMaxLength(32) }
+                }
+                subcommand("list", "Shows all tags of this server.")
+                subcommand("prefix", "Change the prefix to send tags.") {
+                    option<String>(
+                        "prefix", "The prefix to send tags.", true
+                    ) { setMaxLength(16) }
+                    option<Boolean>(
+                        "space", "Add a space behind the prefix.", true
+                    )
+                }
+                subcommand("permissions", "Changes which roles are allowed to send tags.")
+//            subcommand("send", "Sends a tag of this server without any restrictions.") {
+//                option<String>(
+//                    "name", "The name of the tag you want to send.", true
+//                )
+//            }
+            },
+            Command("help", "Get information about Ghastling and how to use it.") {
+                defaultPermissions = DefaultMemberPermissions.enabledFor(
+                    Permission.MANAGE_SERVER
+                )
 
 //            subcommand("send", "Sends a tag of this server without any restrictions.") {
 //                option<String>(
 //                    "name", "The name of the tag you want to send.", true
 //                )
 //            }
-        }).queue()
+            }
+        ).queue()
     }
 }
